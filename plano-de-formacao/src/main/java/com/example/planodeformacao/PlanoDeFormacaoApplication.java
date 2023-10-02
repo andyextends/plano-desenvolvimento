bbe8ce83a915ec0d0267d67af945eaad15ba44f0
Anderson Freitas da Silva <CIANDT\andersonfs@lnb036034cps.ciandt.global>
2023-10-02 09:21:22 -0300

    primeiro commit

---
+++ plano-de-formacao/src/main/java/com/example/planodeformacao/PlanoDeFormacaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.planodeformacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanoDeFormacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanoDeFormacaoApplication.class, args);
+	}
+
+}
---
+++ plano-de-formacao/src/test/java/com/example/planodeformacao/PlanoDeFormacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.planodeformacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanoDeFormacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
