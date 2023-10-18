0901b36aa50a71dbb283b0d8cc3832055f32504f
Anderson Freitas da Silva <CIANDT\andersonfs@lnb036034cps.ciandt.global>
2023-10-18 19:25:57 -0300

    Ajuste de classe de negocio melhorando a regra para tratativa de exception na classe global

--- src/main/java/planodeformacao/produto/exception/GlobalExceptionHandler.java
+++ src/main/java/planodeformacao/produto/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -20,5 +21,10 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<String> handleInternalServerError(Exception ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Algo deu errado. Por favor, tente novamente.");
+    }
+    @ExceptionHandler(ResponseStatusException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<String> handleNotFoundException(ResponseStatusException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Dados de entrada invalido. Por favor, tente novamente.");
     }
 }
