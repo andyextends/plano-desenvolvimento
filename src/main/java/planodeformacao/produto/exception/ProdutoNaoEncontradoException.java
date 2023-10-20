cf0925e28161e2f9b16fe92cb6abefa5c47d2fd9
Anderson Freitas da Silva <CIANDT\andersonfs@lnb036034cps.ciandt.global>
2023-10-20 00:11:52 -0300

    INclusão de novas classes exception para mensagem de erro personalisada

---
+++ src/main/java/planodeformacao/produto/exception/ProdutoNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package planodeformacao.produto.exception;
+
+public class ProdutoNaoEncontradoException extends RuntimeException {
+    public ProdutoNaoEncontradoException(String message) {
+        super(message);
+    }
+}
